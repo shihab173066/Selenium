6ad36877f9ea06d12e6027c3243aa37a9469dbe5
shihab173066 <shihab.hossainSH@gmail.com>
2024-10-04 18:25:31 +0600

    Initial Test Program

---
+++ Interacting With Elements/TestElements.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://www.google.com/")
+
+time.sleep(3)
+print("Done")
