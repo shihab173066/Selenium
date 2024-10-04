8e530250ffbd236f4bde8c9b40f1ed858cb39daf
shihab173066 <shihab.hossainSH@gmail.com>
2024-10-04 18:42:14 +0600

    Automating of google home page completed

--- Interacting With Elements/TestElements.py
+++ Interacting With Elements/TestElements.py
@@ -1,11 +1,17 @@
 import time
 from selenium import webdriver
+from selenium.webdriver import *
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("https://www.google.com/")
+
+googleSearchBox = driver.find_element(By.ID, "APjFqb")
+googleSearchBox.send_keys("Automation")
+googleSearchBox.send_keys(Keys.RETURN)
 
 time.sleep(3)
 print("Done")
