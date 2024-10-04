a1581acb9b3c8e0ed6fdcadefc01e5c9cf325c9d
shihab173066 <shihab.hossainSH@gmail.com>
2024-10-04 20:23:18 +0600

    tested the netlify dot app site

--- Interacting With Elements/TestElements.py
+++ Interacting With Elements/TestElements.py
@@ -7,11 +7,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("https://www.google.com/")
-
-googleSearchBox = driver.find_element(By.ID, "APjFqb")
-googleSearchBox.send_keys("Automation")
-googleSearchBox.send_keys(Keys.RETURN)
-
-time.sleep(3)
+driver.get("https://trytestingthis.netlify.app/index.html/")
+driver.find_element(By.ID, "fname").send_keys("Mr")
+driver.find_element(By.ID, "lname").send_keys("Who")
+time.sleep(5)
+driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+time.sleep(8)
 print("Done")
